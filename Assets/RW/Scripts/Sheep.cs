0212cd23cc30f07922146d817551a6a57e5ae1d2
Liam McKenzie (1000103822) <MCKELD3@student.op.ac.nz>
2023-03-02 12:44:11 +1300

    added sound manager and animations

--- Assets/RW/Scripts/Sheep.cs
+++ Assets/RW/Scripts/Sheep.cs
@@ -14,14 +14,16 @@ public class Sheep : MonoBehaviour
     private Rigidbody myRigidbody; 
 
     private SheepSpawner sheepSpawner;
-    // Start is called before the first frame update
+
+    public float heartOffset; 
+    public GameObject heartPrefab; 
+
     void Start()
     {
         myCollider = GetComponent<Collider>();
         myRigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
     void Update()
     {
         transform.Translate(Vector3.forward * runSpeed * Time.deltaTime);
@@ -29,9 +31,16 @@ public class Sheep : MonoBehaviour
 
     private void HitByHay()
     {
+        SoundManager.Instance.PlaySheepHitClip();
         sheepSpawner.RemoveSheepFromList(gameObject);
         hitByHay = true; 
         runSpeed = 0;
+
+        Instantiate(heartPrefab, transform.position + new Vector3(0, heartOffset, 0), Quaternion.identity);
+        TweenScale tweenScale = gameObject.AddComponent<TweenScale>();
+        tweenScale.targetScale = 0; 
+        tweenScale.timeToReachTarget = gotHayDestroyDelay;
+
 
         Destroy(gameObject, gotHayDestroyDelay);
     }
@@ -51,6 +60,7 @@ public class Sheep : MonoBehaviour
 
     private void Drop()
     {
+        SoundManager.Instance.PlaySheepDroppedClip();
         sheepSpawner.RemoveSheepFromList(gameObject);
         dropped = true;
         myRigidbody.isKinematic = false; 
